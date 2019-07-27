0418065300e48180ed872a4661d27d53af924016
Anton Azhhirevych <azhhirevych943@gmail.com>
2019-07-27 18:16:12 +0300

    Updates

---
+++ precache-manifest.57a89ddef5ae52811f49bb4ea025c934.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "436b1e1bd6b5f1d42d648d156017f2ae",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "b6d9179ac45330b7cd05",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.2f5c8770.chunk.css"
+  },
+  {
+    "revision": "6f1f18b84c8cd63efde5",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.a588a3f5.chunk.js"
+  },
+  {
+    "revision": "b6d9179ac45330b7cd05",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.6ebb50cb.chunk.js"
+  },
+  {
+    "revision": "052338c15f0cfa810fb3",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime~main.a33a0e9d.js"
+  }
+]);
